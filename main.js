43a1621fe93d32c70aca2f35480b77dab931d8eb
ciao-madesign <ciao.madesign@gmail.com>
2026-04-21 20:11:32 +0200

    Change hero image source from hero-index to hero-calc

--- main.js
+++ main.js
@@ -398,7 +398,7 @@
       heroBg.style.backgroundImage = `url('${heroImg.src}')`;
       heroBg.classList.add('hero-bg-loaded');
     };
-    heroImg.src = 'img/hero-index.jpg';
+    heroImg.src = 'img/hero-calc.jpg';
   }
 
   document.getElementById('heroCtaBtn')?.addEventListener('click', (e) => {
